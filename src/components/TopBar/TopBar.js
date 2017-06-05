e5488854d442a4bc7c1ae1b67f054b3bfbe43d43
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-05 18:49:32 +0300

    feat: add TopBar component

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopBar.scss';
+
+export default class TopBar extends React.Component {
+
+  render() {
+    return (
+      <div className='top-bar' />
+    );
+  }
+
+}
--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
+import TopBar from '../../components/TopBar/TopBar';
+
 import PropTypes from 'prop-types';
 import './PageLayout.scss';
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
+    <TopBar />
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
