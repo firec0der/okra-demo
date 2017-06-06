7c43f905051645c15bf3986a11792b9878bbcf53
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-06 23:35:10 +0300

    feat: add UL logo, setup SearchBar and logo on the main page.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,24 +1,37 @@
 // import from vendors
 import React from 'react';
+import { Grid, Col } from 'react-bootstrap';
 
 // import from components
 import BarChart from '../../components/BarChart/BarChart';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 // import from styles
 import './HomePage.scss';
 
+// import from assets
+import UnileverLargeLogo from '../../assets/images/UL-large-logo.png';
+
+const barChartData = [
+  { name: 'Unilever', 2007: 29.1, 2008: 31.28, 2009: 39.62 },
+  { name: 'P&G', 2007: 14.63, 2008: 13.04, 2009: 10.1 },
+  { name: 'Beiersdorf', 2007: 20.3, 2008: 18.9, 2009: 25.96 },
+];
+
 export default class HomePage extends React.Component {
 
   render() {
-    const barChartData = [
-      { name: 'Unilever', 2007: 29.1, 2008: 31.28, 2009: 39.62 },
-      { name: 'P&G', 2007: 14.63, 2008: 13.04, 2009: 10.1 },
-      { name: 'Beiersdorf', 2007: 20.3, 2008: 18.9, 2009: 25.96 },
-    ];
+    const searchOnSubmit = (value) => window.alert(`It works, value: ${value}`);
 
     return (
-      <div>
-        <BarChart data={barChartData} />
+      <div className='home-page'>
+        <Grid className='top-content'>
+          <Col xs={12} md={6} mdOffset={3}>
+            <img className='unilever-logo' src={UnileverLargeLogo} />
+            <SearchBar onSubmit={searchOnSubmit} />
+          </Col>
+        </Grid>
+        { /* <BarChart data={barChartData} /> */ }
       </div>
     );
   }
