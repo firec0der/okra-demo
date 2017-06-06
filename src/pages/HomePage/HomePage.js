6d73957fcf591bf1d73baf547ce45d732fe88737
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-07 01:39:34 +0300

    feat: display BarChart and PieChart on the Home page.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Grid, Col } from 'react-bootstrap';
 
 // import from components
-import BarChart from '../../components/BarChart/BarChart';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import BarChart from '../../components/BarChart/BarChart';
+import PieChart from '../../components/PieChart/PieChart';
 
 // import from styles
 import './HomePage.scss';
@@ -16,6 +17,11 @@ const barChartData = [
   { name: 'Unilever', 2007: 29.1, 2008: 31.28, 2009: 39.62 },
   { name: 'P&G', 2007: 14.63, 2008: 13.04, 2009: 10.1 },
   { name: 'Beiersdorf', 2007: 20.3, 2008: 18.9, 2009: 25.96 },
+];
+
+const pieChartData = [
+  { name: 'Sector A', value: 400 }, { name: 'Sector B', value: 300 },
+  { name: 'Sector C', value: 300 }, { name: 'Sector D', value: 200 }
 ];
 
 export default class HomePage extends React.Component {
@@ -31,7 +37,18 @@ export default class HomePage extends React.Component {
             <SearchBar onSubmit={searchOnSubmit} />
           </Col>
         </Grid>
-        { /* <BarChart data={barChartData} /> */ }
+
+        <Grid>
+          <Col xs={12} md={6} mdOffset={3}>
+            <BarChart data={barChartData} />
+          </Col>
+        </Grid>
+
+        <Grid>
+          <Col xs={12} md={6} mdOffset={3}>
+            <PieChart data={pieChartData} />
+          </Col>
+        </Grid>
       </div>
     );
   }
