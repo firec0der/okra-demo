fb38e809b01457e88b59b0cd2a5fbd5052f4a3e5
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-11 17:50:55 +0300

    feat: fetch kantar brands on home page enter.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,15 @@ import CoreLayout from '../layouts/PageLayout/PageLayout';
 
 import HomePage from '../pages/HomePage/HomePage';
 
+import { fetchKantarBrands } from '../modules/kantarBrands';
+
+export const onHomeEnter = ({ dispatch }) => ({ params }) => {
+  dispatch(fetchKantarBrands(params.slug));
+};
+
 export default (store) => ({
   path: '/',
   component: CoreLayout,
-  indexRoute: { component: HomePage },
+  indexRoute: { component: HomePage, onEnter: onHomeEnter(store) },
   // childRoutes: []
 });
