e16c5a870c17cd7d75a04e3156a4a6fff2e3e152
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-20 17:31:38 +0300

    feat: add KantarPeriodsBarChart container.

---
+++ src/containers/KantarPeriodsBarChart/dataFiltersPropTypes.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default {
+  kantarAreas: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    dictionary: PropTypes.object.isRequired,
+  }),
+  kantarBrands: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    dictionary: PropTypes.object.isRequired,
+  }),
+  kantarGenres: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    dictionary: PropTypes.object.isRequired,
+    applicableForBrands: PropTypes.arrayOf(PropTypes.number).isRequired,
+  }),
+  kantarLevels: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    dictionary: PropTypes.object.isRequired,
+  }),
+  kantarPackagings: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    dictionary: PropTypes.object.isRequired,
+    applicableForBrands: PropTypes.arrayOf(PropTypes.number).isRequired,
+  })
+};
