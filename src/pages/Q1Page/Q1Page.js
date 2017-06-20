cf52742f79c7361603d14825a52ceaf1718735b7
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-21 00:56:58 +0300

    feat: remove unstable Nielsen line chart rendering, render PeriodsBarChart.

--- src/pages/Q1Page/Q1Page.js
+++ src/pages/Q1Page/Q1Page.js
@@ -4,60 +4,52 @@ import { Grid, Col } from 'react-bootstrap';
 
 // import from constants
 import {
-  NIELSEN_LEVEL_FILTER,
-  NIELSEN_CHANNEL_FILTER,
-  NIELSEN_AREA_FILTER,
-  NIELSEN_APPLIER_FILTER,
-  NIELSEN_GENRE_FILTER,
-  NIELSEN_PACKAGING_FILTER,
-  NIELSEN_DATA_FILTERS_CONFIG
-} from '../../constants/nielsenDataFilters';
+  LEVEL_FILTER,
+  CHANNEL_FILTER,
+  AREA_FILTER,
+  GENRE_FILTER,
+  PACKAGING_FILTER,
+  DATA_FILTERS_CONFIG
+} from '../../constants/dataFilters';
 
 // import from containers
-import NielsenPeriodsBarChart from '../../containers/NielsenPeriodsBarChart/NielsenPeriodsBarChart';
-import NielsenPeriodsLineChart from '../../containers/NielsenPeriodsLineChart/NielsenPeriodsLineChart';
+import PeriodsBarChart from '../../containers/PeriodsBarChart/PeriodsBarChart';
 
 export default class Q1Page extends React.Component {
 
   render() {
-    const dataFilters = [
-      NIELSEN_AREA_FILTER,
-      NIELSEN_APPLIER_FILTER,
-      NIELSEN_GENRE_FILTER,
-      NIELSEN_PACKAGING_FILTER
+    const barChartDataFilters = [
+      AREA_FILTER,
+      GENRE_FILTER,
+      PACKAGING_FILTER
     ];
 
-    const requiredFilters = [
-      NIELSEN_AREA_FILTER
+    const barChartRequiredFilters = [
+      AREA_FILTER
     ];
 
-    const values = {
-      [NIELSEN_DATA_FILTERS_CONFIG[NIELSEN_CHANNEL_FILTER].key]: 1,
-      [NIELSEN_DATA_FILTERS_CONFIG[NIELSEN_LEVEL_FILTER].key]: 2,
-      [NIELSEN_DATA_FILTERS_CONFIG[NIELSEN_GENRE_FILTER].key]: 1,
-      [NIELSEN_DATA_FILTERS_CONFIG[NIELSEN_AREA_FILTER].key]: [1],
+    const barChartValues = {
+      [DATA_FILTERS_CONFIG[CHANNEL_FILTER].key]: 1,
+      [DATA_FILTERS_CONFIG[LEVEL_FILTER].key]: 2,
+      [DATA_FILTERS_CONFIG[GENRE_FILTER].key]: 1,
+      [DATA_FILTERS_CONFIG[AREA_FILTER].key]: [1],
     };
 
     return (
       <div className='home-page'>
         <Grid style={{ marginBottom: '30px' }}>
           <Col xs={12} md={8} mdOffset={2}>
-            <h4 className="text-center">
+            <h4 className='text-center'>
               What is the market share of female deodorants in supermarkets?
             </h4>
           </Col>
         </Grid>
 
-        <NielsenPeriodsBarChart
-          dataFilters={dataFilters}
-          values={values}
-          requiredFilters={requiredFilters}
-        />
-
-        <NielsenPeriodsLineChart
-          dataFilters={dataFilters}
-          values={values}
-          requiredFilters={requiredFilters}
+        <PeriodsBarChart
+          dataFilters={barChartDataFilters}
+          values={barChartValues}
+          requiredFilters={barChartRequiredFilters}
+          chosenMetric='weightedDistribution'
         />
       </div>
     );
