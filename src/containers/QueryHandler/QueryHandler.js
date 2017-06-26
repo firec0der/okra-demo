a42d53cd987336a9bdb9ff7fdfd45b7b4cc64ac8
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-26 04:29:53 +0300

    feat: handle areas in the query.

--- src/containers/QueryHandler/QueryHandler.js
+++ src/containers/QueryHandler/QueryHandler.js
@@ -39,8 +39,9 @@ class QueryHandler extends React.Component {
     super(...args);
 
     this.state = {
+      parsedAreas: [],
       parsedBrands: [],
-      parsedGenre: null
+      parsedGenre: null,
     };
   }
 
@@ -62,8 +63,33 @@ class QueryHandler extends React.Component {
       .find(id => lowerCasedQuery.includes(genres[id]));
   };
 
+  detectArea = query => {
+    const dictionary = {
+      // NORTH+NORTHEAST
+      1: ['Area I', 'AreaI', 'Area1', 'Area 1', 'North', 'North East', 'northeast', 'northeastern', 'ceara', 'rio grande do norte', 'paraiba', 'Pernambuco', 'Alagoas', 'Bahia', 'Sergipe', 'Piaui', 'Maranhao'],
+      // EAST
+      2: ['Area II', 'AreaII', 'Area2', 'Area 2', 'south east', 'southeast', 'south_east'],
+      // METROP. RIO
+      3: ['Area III', 'AreaIII', 'Area 3', 'Area3', 'Rio', 'Rio de janeiro', 'Janeiro', 'metopolian region rio', 'metrop rio', 'riodejaneiro', 'rio d janeiro'],
+      // METROP. SP
+      4: ['Area IV', 'AreaIV', 'Area 4', 'Area4', 'São Paulo', 'SãoPaulo', 'Sao', 'Paulo', 'meropolian region SP', 'SP'],
+      // SP COUNTRYSIDE
+      5: ['Area V', 'AreaV', 'Area 5', 'Area5', 'Interior of São Paulo', 'Interior SP', 'Countryside SP', 'Country SP'],
+      // SOUTH
+      6: ['Area VI', 'AreaVI', 'Area 6', 'Area6', 'SOUTH', 'Paraná', 'Santa Catarina', 'Rio Grande do Sul', 'Parana', 'SantaCatarina', 'RioGrande do Sul'],
+      7: ['MIDWEST'],
+      8: ['T BRAZIL']
+    };
+
+    const lowerCasedQuery = query.toLowerCase();
+
+    return Object
+      .keys(dictionary)
+      .filter(id => dictionary[id].some(keyWord => lowerCasedQuery.includes(keyWord.toLowerCase())));
+  }
+
   getBarChart = () => {
-    const { parsedBrands, parsedGenre } = this.state;
+    const { parsedAreas, parsedBrands, parsedGenre } = this.state;
 
     const barChartDataFilters = [
       BRAND_FILTER,
@@ -78,7 +104,7 @@ class QueryHandler extends React.Component {
 
     const barChartValues = {
       [DATA_FILTERS_CONFIG[BRAND_FILTER].key]: parsedBrands.map(brand => brand.id),
-      [DATA_FILTERS_CONFIG[AREA_FILTER].key]: [8],
+      [DATA_FILTERS_CONFIG[AREA_FILTER].key]: parsedAreas.length || [8],
       [DATA_FILTERS_CONFIG[GENRE_FILTER].key]: parsedGenre,
     };
 
@@ -94,8 +120,9 @@ class QueryHandler extends React.Component {
   };
 
   searchOnSubmit = value => this.setState({
+    parsedAreas: this.detectArea(value).map(id => parseInt(id)),
     parsedBrands: this.detectBrand(value),
-    parsedGenre: this.detectGenre(value)
+    parsedGenre: this.detectGenre(value),
   });
 
   render() {
