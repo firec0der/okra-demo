deaef893ae727e23277bc02a8a6c3efb485e9f9c
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-06-29 12:59:44 +0300

    fix: change text of chart message.

--- src/containers/Kantar/PeriodsBarChart/KantarPeriodsBarChart.js
+++ src/containers/Kantar/PeriodsBarChart/KantarPeriodsBarChart.js
@@ -208,7 +208,7 @@ class KantarPeriodsBarChart extends React.Component {
         return `The ${growthVariables[metric].text} of ` +
           `${packagingValue ? packagingValue : ''} ` +
           `${genreValue ? genreValue : ''} ${brandName} ` +
-          `is ${status} in the last quarter by a rate of ${Math.abs(growthValue.toFixed(1))}%`;
+          `${status} in the last quarter by ${Math.abs(growthValue.toFixed(1))}%`;
       });
 
     const colStyles = { marginBottom: '30px', paddingTop: '10px', paddingBottom: '10px', backgroundColor: '#fff' };
