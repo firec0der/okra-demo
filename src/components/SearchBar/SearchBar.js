4e7c4bc7e88e6684d2ad4bfbe3128b6244d458b6
Ivan Stetsenko <i.stetsenko1@gmail.com>
2017-07-04 14:40:06 +0300

    feat: add voice recognition in webkit.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
+import _ from 'lodash/fp';
+
+import MicrophoneIcon from './microphone.png';
 
 export default class SearchBar extends React.Component {
 
@@ -17,7 +20,8 @@ export default class SearchBar extends React.Component {
     value: '',
     onChange: (value) => {},
     onSubmit: (value) => {},
-    buttonText: 'Search'
+    buttonText: 'Search',
+    recognition: false
   }
 
   constructor(props, ...args) {
@@ -28,18 +32,35 @@ export default class SearchBar extends React.Component {
 
   onSubmit = () => this.props.onSubmit(this.state.value);
 
-  onChange = (event) => {
+  onChange = event => {
     const { value } = event.target;
     this.setState({ value }, this.props.onChange.bind(null, value));
-  }
+  };
 
-  onKeyPress = (event) => {
+  onKeyPress = event => {
     if (event.key === 'Enter') { this.onSubmit(); }
-  }
+  };
+
+  onSpeechRecognition = event => {
+    this.setState({ recognition: true }, () => {
+      const recognition = new window.webkitSpeechRecognition();
+
+      recognition.onresult = (event) => {
+        const transcript = _.getOr('', 'results[0][0].transcript', event);
+
+        transcript && this.setState(
+          { value: transcript, recognition: false },
+          this.props.onChange.bind(null, transcript)
+        );
+      };
+
+      recognition.start();
+    });
+  };
 
   render() {
     const { buttonText, placeholder } = this.props;
-    const { value } = this.state;
+    const { value, recognition } = this.state;
 
     const inputProps = {
       placeholder,
@@ -55,6 +76,15 @@ export default class SearchBar extends React.Component {
           <InputGroup>
             <FormControl type='text' {...inputProps} />
             <InputGroup.Button>
+              { window.webkitSpeechRecognition && (
+                <Button
+                  style={{ borderColor: '#007dbb', color: '#007dbb' }}
+                  onClick={this.onSpeechRecognition}
+                  disabled={recognition}
+                >
+                  <img src={MicrophoneIcon} style={{ height: '18px' }} />
+                </Button>
+              ) }
               <Button style={{ borderColor: '#007dbb', color: '#007dbb' }} onClick={this.onSubmit}>
                 { buttonText }
               </Button>
