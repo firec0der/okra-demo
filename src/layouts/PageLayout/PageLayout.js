76de134a29f28c37e1345ccc035b386f4bf42c8e
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 22:53:51 +0200

    copy old PageLayout

---
+++ src/layouts/PageLayout/PageLayout.js
@@ -0,0 +1,31 @@
+// import from vendors
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import from components
+import Header from '../../components/Header/Header';
+
+// import from styles
+import './PageLayout.scss';
+
+export default class PageLayout extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div className='page-layout'>
+        <Header />
+        <main className='main'>
+          { children }
+        </main>
+        <footer className='footer' />
+      </div>
+    );
+  }
+
+}
