41aa1392e7a1d1954fc9db20e4416ec04ecc446b
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 22:15:58 +0200

    add store.js

---
+++ src/store.js
@@ -0,0 +1,37 @@
+// import from vendors
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const enhancers = [];
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  const { devToolsExtension } = window;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
+
+const store = createStore(
+  rootReducer,
+  {},
+  composedEnhancers
+);
+
+if (process.env.NODE_ENV === 'development') {
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      store.replaceReducer(require('./reducers').default); // eslint-disable-line global-require
+    });
+  }
+}
+
+export default store;
