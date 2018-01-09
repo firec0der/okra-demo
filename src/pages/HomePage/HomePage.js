83e680d65074259add5b688dc9f132bccf859c76
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 23:30:45 +0200

    copy old pages

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,30 @@
+// import from vendors
+import React from 'react';
+import { Grid, Col } from 'react-bootstrap';
+
+// import from containers
+import QueryHandler from '../../containers/QueryHandler/QueryHandler';
+
+// import from styles
+import './HomePage.css';
+
+// import from assets
+import UnileverLargeLogo from './UL-large-logo.png';
+
+export default class HomePage extends React.Component {
+
+  render() {
+    return (
+      <div className='home-page'>
+        <Grid className='top-content' key='top-content'>
+          <Col xs={12} md={6} mdOffset={3}>
+            <img className='unilever-logo' src={UnileverLargeLogo} />
+          </Col>
+        </Grid>
+
+        <QueryHandler key='query-handler' />
+      </div>
+    );
+  }
+
+}
---
+++ src/pages/Q1Page/Q1Page.js
@@ -0,0 +1,75 @@
+// import from vendors
+import React from 'react';
+import { Grid, Col } from 'react-bootstrap';
+
+// import from constants
+import {
+  CHANNEL_FILTER,
+  BRAND_FILTER,
+  AREA_FILTER,
+  GENRE_FILTER,
+  PACKAGING_FILTER,
+  SUBCATEGORY_FILTER,
+  DATA_FILTERS_CONFIG
+} from '../../constants/dataFilters';
+
+// import from containers
+import PeriodsBarChart from '../../containers/PeriodsBarChart/PeriodsBarChart';
+import PeriodsLineChart from '../../containers/PeriodsLineChart/PeriodsLineChart';
+
+export default class Q1Page extends React.Component {
+
+  render() {
+    const barChartDataFilters = [
+      SUBCATEGORY_FILTER,
+      BRAND_FILTER,
+      AREA_FILTER,
+      CHANNEL_FILTER,
+      GENRE_FILTER,
+      PACKAGING_FILTER
+    ];
+
+    const barChartRequiredFilters = [];
+
+    const barChartValues = {
+      [DATA_FILTERS_CONFIG[CHANNEL_FILTER].key]: 1,
+      [DATA_FILTERS_CONFIG[AREA_FILTER].key]: [8],
+      [DATA_FILTERS_CONFIG[BRAND_FILTER].key]: [2, 3, 4],
+    };
+
+    const lineChartDataFilters = [
+      SUBCATEGORY_FILTER,
+      BRAND_FILTER,
+      AREA_FILTER,
+      CHANNEL_FILTER,
+      GENRE_FILTER,
+      PACKAGING_FILTER
+    ];
+
+    const lineChartRequiredFilters = [];
+
+    const lineChartValues = {
+      [DATA_FILTERS_CONFIG[CHANNEL_FILTER].key]: 1,
+      [DATA_FILTERS_CONFIG[AREA_FILTER].key]: [8],
+      [DATA_FILTERS_CONFIG[BRAND_FILTER].key]: [2, 3, 4],
+    };
+
+    return (
+      <div className='home-page'>
+        <PeriodsBarChart
+          dataFilters={barChartDataFilters}
+          dataFiltersValues={barChartValues}
+          requiredFilters={barChartRequiredFilters}
+          chosenMetric='weightedDistribution'
+        />
+        <PeriodsLineChart
+          dataFilters={lineChartDataFilters}
+          dataFiltersValues={lineChartValues}
+          requiredFilters={lineChartRequiredFilters}
+          chosenMetric='marketShareGrowth'
+        />
+      </div>
+    );
+  }
+
+}
