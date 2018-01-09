f004d322d687bfc162ae554b3b61050b4a8565c8
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 22:12:24 +0200

    combine reducers

---
+++ src/reducers.js
@@ -0,0 +1,53 @@
+import { combineReducers } from 'redux';
+
+import metrics from './modules/metrics';
+import brands from './modules/brands';
+import manufacturers from './modules/manufacturers';
+
+import kantarAreas from './modules/kantar/kantarAreas';
+import kantarBrands from './modules/kantar/kantarBrands';
+import kantarGenres from './modules/kantar/kantarGenres';
+import kantarPackagings from './modules/kantar/kantarPackagings';
+import kantarSubcategories from './modules/kantar/kantarSubcategories';
+import kantarManufacturers from './modules/kantar/kantarManufacturers';
+
+import nielsenAppliers from './modules/nielsen/nielsenAppliers';
+import nielsenAreas from './modules/nielsen/nielsenAreas';
+import nielsenBrands from './modules/nielsen/nielsenBrands';
+import nielsenChannels from './modules/nielsen/nielsenChannels';
+import nielsenGenres from './modules/nielsen/nielsenGenres';
+import nielsenManufacturers from './modules/nielsen/nielsenManufacturers';
+import nielsenPackagings from './modules/nielsen/nielsenPackagings';
+import nielsenSubcategories from './modules/nielsen/nielsenSubcategories';
+
+import nwbBrands from './modules/nwb/nwbBrands';
+import nwbGenres from './modules/nwb/nwbGenres';
+import nwbManufacturers from './modules/nwb/nwbManufacturers';
+import nwbSubcategories from './modules/nwb/nwbSubcategories';
+
+export default combineReducers({
+  metrics,
+  brands,
+  manufacturers,
+
+  kantarAreas,
+  kantarBrands,
+  kantarGenres,
+  kantarManufacturers,
+  kantarPackagings,
+  kantarSubcategories,
+
+  nielsenAppliers,
+  nielsenAreas,
+  nielsenBrands,
+  nielsenChannels,
+  nielsenGenres,
+  nielsenManufacturers,
+  nielsenPackagings,
+  nielsenSubcategories,
+
+  nwbBrands,
+  nwbGenres,
+  nwbManufacturers,
+  nwbSubcategories
+});
