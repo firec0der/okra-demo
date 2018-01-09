4e0d4920b9703c8ac835c6254b6f7cbaaf1f4631
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 21:49:00 +0200

    enable HMR, import whatwg-fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// imports from vendors
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'whatwg-fetch';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// imports from styles
+
+import App from './App';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(<App />, root);
+  });
+}
