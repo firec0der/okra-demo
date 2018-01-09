966d8b670c511193df3ffc3d0b7ef67c86f0403c
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-01-09 23:12:25 +0200

    render PageLayout, include main.scss styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-// import from vendors
+// imports from vendors
 import React from 'react';
 import { Provider } from 'react-redux';
 
-// import from store
+// imports from layouts
+import PageLayout from './layouts/PageLayout/PageLayout';
+
+// imports from store
 import store from './store';
+
+// imports from styles
+import './styles/main.css';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <Provider store={store}>
-        <div>Hello</div>
+        <PageLayout>
+          <div>Hello</div>
+        </PageLayout>
       </Provider>
     );
   }
